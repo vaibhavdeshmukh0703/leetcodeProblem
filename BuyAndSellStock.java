beee8a68266d762edb209f53fde908426ebce559
vaibhav <vaibhavdeshmukh0703@gmail.com>
2022-06-17 23:04:40 +0530

    New Changes In BuyAnd Sell Problem

--- BuyAndSellStock.java
+++ BuyAndSellStock.java
@@ -1,45 +1,43 @@
 public class BuyAndSellStock {
     public int maxProfit(int[] prices) {
         // loop trhue all array and find the min price buy on that day.
-         int minPrice = prices[0];
-         int indexOfMinvalueShare = 0;
+
+        int minPrice = prices[0];
+        int indexOfMinvalueShare = 0;
         int maxPrice = 0;
         int MaxProfit = 0;
-        if(prices.length == 1){
+
+        if (prices.length == 1) {
             return 0;
         }
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]<minPrice){
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
                 minPrice = prices[i];
                 indexOfMinvalueShare = i;
                 maxPrice = minPrice;
             }
-         
         }
-       
-    System.out.println("Min Price "+minPrice+" and index is "+indexOfMinvalueShare);
         
         // loop though min price to remain array to find max price day to sell share.
-         if(indexOfMinvalueShare!= prices.length-1){
-            for(int j=indexOfMinvalueShare;j<prices.length;j++){
-                if(prices[j]>maxPrice){
+        if (indexOfMinvalueShare != prices.length - 1) {
+            for (int j = indexOfMinvalueShare; j < prices.length; j++) {
+                if (prices[j] > maxPrice) {
                     maxPrice = prices[j];
                 }
             }
-        }
-        else{
+        } else {
             return MaxProfit = 0;
         }
-         System.out.println("max Price "+maxPrice);
+        
         MaxProfit = maxPrice - minPrice;
 
-        System.out.println("Max Profit is "+MaxProfit);
         return MaxProfit;
     }
-   public static void main(String[] args) {
-    BuyAndSellStock bc = new BuyAndSellStock();
-    int [] prices = {7,3,5,1};
-   int result =  bc.maxProfit(prices);
-   System.out.println("Max profite is "+result);
-   } 
+
+    public static void main(String[] args) {
+        BuyAndSellStock bc = new BuyAndSellStock();
+        int[] prices = { 7, 3, 5, 1 };
+        int result = bc.maxProfit(prices);
+        System.out.println("Max profite is " + result);
+    }
 }
